89ffabc859c117a0f5efe2caaa6b166b86c03168
Martin-Nordeback <martin.nordeback@gmail.com>
2023-04-13 11:27:11 +0200

    Initial Commit

---
+++ MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by Martin Nordebäck on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
